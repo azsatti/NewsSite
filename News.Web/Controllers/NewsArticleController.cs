e710aa3feb331774ce8af2b461aad8f9b26655ce
aftab_zaheer <aftab.zaheer@share.co.uk>
2018-05-18 16:51:04 +0100

    adding website project

---
+++ News.Web/Controllers/NewsArticleController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using News.Entity;
+using News.Web.Interfaces;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace News.Web.Controllers
+{
+    public class NewsArticleController : Controller
+    {
+        private readonly IApiClient _apiClient;
+
+        public NewsArticleController(IApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            var responseMessage = await _apiClient.GetAsync("news");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = await responseMessage.Content.ReadAsStringAsync();
+
+                var newsArticles = JsonConvert.DeserializeObject<List<NewsArticle>>(responseData);
+
+                return View(newsArticles);
+            }
+            return View("Error");
+        }
+    }
+}
---
+++ News.Web/Utility/ApiClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using News.Web.Interfaces;
+
+namespace News.Web.Utility
+{
+    public class ApiClient : IApiClient
+    {
+        readonly HttpClient _client = new HttpClient();
+
+        public ApiClient(IConfiguration configuration)
+        {
+            var apiUrl = configuration["APIURL"];
+
+            _client.BaseAddress = new Uri(apiUrl);
+
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+
+        public async Task<HttpResponseMessage> GetAsync(string url)
+        {
+            return await _client.GetAsync(url);
+        }
+    }
+}
